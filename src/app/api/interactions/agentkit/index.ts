b5228a15b71b61b43c4df3e9b923392f4082fea7
chengggkk <chengggkk@gmail.com>
2025-02-02 14:51:00 +0800

    agentkit retrieve wallet address

--- src/app/api/interactions/agentkit/index.ts
+++ src/app/api/interactions/agentkit/index.ts
@@ -1,6 +1,7 @@
 import { initializeAgent } from "./agent";
 import { HumanMessage } from "@langchain/core/messages";
 import { sendMessage } from "./edit";
+import { string } from "zod";
 
 const maxLength = 1500;
 
@@ -33,8 +34,8 @@ export function splitContent(content: string): string[] {
     return chunks;
 }
 
-export const agentkit = async (channel_id: string, options: any) => {
-    const { agent, config } = await initializeAgent();
+export const agentkit = async (channel_id: string, options: any, userId:string) => {
+    const { agent, config } = await initializeAgent(userId);
     const stream = await agent.stream({ messages: [new HumanMessage(options[0].value)] }, config);
     for await (const chunk of stream) {
         if ("agent" in chunk) {
