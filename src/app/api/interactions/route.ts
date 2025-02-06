2c72f0448a7c5ba8db1b01b48cf17db8745f36f7
chengggkk <chengggkk@gmail.com>
2025-02-06 11:31:24 +0800

    format

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
         }
         const userId = member?.user?.id || user?.id;
 
-
+        
         if (type === InteractionType.APPLICATION_COMMAND) {
             const { name, options, custom_id, id, resolved } = data;
 
@@ -210,9 +210,10 @@ export async function POST(request: NextRequest) {
             }
 
 
-        } else if (type === InteractionType.MODAL_SUBMIT) {
-        }
-
+        }else if (type === InteractionType.MODAL_SUBMIT) {
+        
+        
+    }
 
 
         else {
